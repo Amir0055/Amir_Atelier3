dfea7d8399c8e769813f698c708b4f946b594806
Amir0055 <93008466+Amir0055@users.noreply.github.com>
2023-02-27 08:55:48 +0800

    comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,26 @@
+import { Component,OnInit } from "@angular/core";
+import { driven } from "../MODEL/driven";
+
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.css']
+})
+export class TemplateDrivenFormComponent implements OnInit {
+   
+   Driven!:driven;
+   titreg="Informations générales ";
+   titrep="Informations de payement ";
+  constructor() { }
+
+  ngOnInit(): void {
+    this.Driven= new driven();
+    
+  }
+    ajouter(){
+      console.log(this.Driven)
+    }
+  
+
+}
