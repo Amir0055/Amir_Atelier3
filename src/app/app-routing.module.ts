42222d6ded652ae79fdfdd72a3be3cde1f12a6fd
Amir0055 <93008466+Amir0055@users.noreply.github.com>
2023-03-17 19:03:27 +0100

    Atelier Interaction entre composants
    Rendu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActorsComponent } from './actors/actors.component';
+import { ConvertisseurComponent } from './convertisseur/convertisseur.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:"Exercice1",pathMatch:'full'},
+  {path:'Exercice1', component : ConvertisseurComponent},
+  {path:'Exercice2', component : ActorsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ConvertisseurComponent } from './convertisseur/convertisseur.component';
+import { ToEuroConvertComponent } from './to-euro-convert/to-euro-convert.component';
+import { ActorsComponent } from './actors/actors.component';
+import { ActorComponent } from './actor/actor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenFormComponent
+    ConvertisseurComponent,
+    ToEuroConvertComponent,
+    ActorsComponent,
+    ActorComponent
   ],
   imports: [
     BrowserModule,
